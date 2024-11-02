93e0bdbd93802e598bc3b0be2a4c2c5c57a45532
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-02 19:41:21 +0900

    コメント機能修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,20 @@
 class CommentsController < ApplicationController
+
   def create
     Comment.create(comment_params)
+    redirect_to request.referer
   end
+
   def destroy
-    
-    # binding.pry
-    
     comment = Comment.find(params[:id])
     comment.destroy
-    redirect_to profile_path(params[:profile_id])
+    redirect_to request.referer
   end
+
   private
+
   def comment_params
-    params.require(:comment).permit(:text).merge(profile_id: params[:profile_id], sound_id: params[:sound_id])
+    params.require(:comment).permit(:text).merge(profile_id: session[:current_profile_id], sound_id: params[:sound_id])
   end
+
 end
