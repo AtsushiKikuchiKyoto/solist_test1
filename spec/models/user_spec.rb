4052f6b8756fa64ee54e0ff1662f3fc4bacce3a2
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-04 17:58:49 +0900

    Userモデルテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー新規登録' do
+    context "登録できるとき" do
+      it '正常にemailとpasswordが入力される' do
+        @user = FactoryBot.build(:user)
+        @user.valid?
+        expect(@user).to be_valid
+      end
+    end
+    context "登録できないとき" do
+      it 'emailが空' do
+        @user = FactoryBot.build(:user)
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Eメールを入力してください"
+      end
+      it 'passwordが空' do
+        @user = FactoryBot.build(:user)
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
+      end
+      it 'password_confirmationが空' do
+        @user = FactoryBot.build(:user)
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
+      end
+      it 'passwordと確認が異なる' do
+        @user = FactoryBot.build(:user)
+        @user.password =              'abcde12345'
+        @user.password_confirmation = 'abcde1234'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
+      end
+    end
+  end
 end
