719396221ae2391e678da46302ff3b6b6bff290a
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-05 13:41:47 +0900

    profileモデルテスト

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @profile = FactoryBot.build(:profile)
+  end
+  describe 'profile新規作成' do
+    context "作成できるとき" do
+      it 'すべての項目が正常に入力される' do
+        @profile.valid?
+        expect(@profile).to be_valid
+      end
+    end
+    context "作成できないとき" do
+      it 'nameが空の時' do
+        @profile.name = ""
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include "名前を入力してください"
+      end
+      it 'textが空の時' do
+        @profile.text = ""
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include "紹介文を入力してください"
+      end
+      it '画像が空の時' do
+        @profile.image = nil
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include "アバター画像を入力してください"
+      end
+      it '画像が500kb以上のとき' do
+        @profile.image.attach(io: File.open('spec/files/over_size_avatar.jpg'), filename: 'over_size_avatar.jpg')
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include "アバター画像ファイル サイズは 500KB 未満にする必要があります (現在のサイズは 1.22MB)"
+      end
+      it '対応していない画像拡張子のとき' do
+        @profile.image.attach(io: File.open('spec/files/unsupported_format_avatar.webp'), filename: 'unsupported_format_avatar.webp')
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include "アバター画像のContent Typeが不正です"
+      end
+      it '画像に音声ファイルを添付したとき' do
+        @profile.image.attach(io: File.open('spec/files/sound.m4a'), filename: 'sound.m4a')
+        @profile.valid?
+        expect(@profile.errors.full_messages).to include "アバター画像のContent Typeが不正です"
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,32 +7,27 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context "登録できるとき" do
       it '正常にemailとpasswordが入力される' do
-        @user = FactoryBot.build(:user)
         @user.valid?
         expect(@user).to be_valid
       end
     end
     context "登録できないとき" do
       it 'emailが空' do
-        @user = FactoryBot.build(:user)
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "Eメールを入力してください"
       end
       it 'passwordが空' do
-        @user = FactoryBot.build(:user)
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
       end
       it 'password_confirmationが空' do
-        @user = FactoryBot.build(:user)
         @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
       end
       it 'passwordと確認が異なる' do
-        @user = FactoryBot.build(:user)
         @user.password =              'abcde12345'
         @user.password_confirmation = 'abcde1234'
         @user.valid?
