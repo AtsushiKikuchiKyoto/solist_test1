4d1ff4247d903f2d36c46b65b702e3157a214d65
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-05 18:38:57 +0900

    Userシステムテスト

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+    @user2 = FactoryBot.create(:user)
+  end
+  describe "システムテスト" do
+    context "user登録できるとき" do
+      it "正しい情報を入力" do
+        visit new_user_registration_path
+        expect(page).to have_content('新規登録')
+        fill_in 'email', with: @user.email
+        fill_in 'password', with: @user.password
+        fill_in 'password_confirmation', with: @user.password_confirmation
+        expect{ 
+          click_on 'submit'
+          sleep 1
+         }.to change { User.count }.by(1)
+        expect(page).to have_current_path(root_path)
+      end
+    end
+    context "user登録できないとき" do
+      it "誤った情報入力によりページに戻ってくる" do
+        visit new_user_registration_path
+        expect(page).to have_content('新規登録')
+        fill_in 'email', with: @user.email
+        fill_in 'password', with: @user.password
+        fill_in 'password_confirmation', with: ''
+        expect{ 
+          click_on 'submit'
+          sleep 1
+         }.to change { User.count }.by(0)
+        expect(page).to have_current_path(new_user_registration_path)
+      end
+    end
+    context "ログインできるとき" do
+      it "正しい情報を入力" do
+        visit new_user_session_path
+        expect(page).to have_content('ログイン')
+        fill_in 'email', with: @user2.email
+        fill_in 'password', with: @user2.password
+        click_on 'submit'
+        expect(page).to have_current_path(root_path)
+      end
+    end
+    context "ログインできないとき" do
+      it "誤った情報入力によりページに戻ってくる" do
+        visit new_user_session_path
+        expect(page).to have_content('ログイン')
+        fill_in 'email', with: @user2.email
+        fill_in 'password', with: ''
+        click_on 'submit'
+        expect(page).to have_current_path(new_user_session_path)
+      end
+    end
+  end
+end
