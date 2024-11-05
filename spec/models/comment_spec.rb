1bc1d72f4162f5d4d75b064f8f209e889b4dbae9
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-05 14:01:27 +0900

    コメントモデルテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  describe 'comment投稿' do
+    context "投稿できるとき" do
+      it 'textが正常に入力される' do
+        @comment.valid?
+        expect(@comment).to be_valid
+      end
+    end
+    context "投稿できないとき" do
+      it 'textが空' do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "コメントを入力してください"
+      end
+      it 'profileが選択されていない' do
+        @comment.profile = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "プロフィールを入力してください"
+      end
+    end
+  end
+
 end
