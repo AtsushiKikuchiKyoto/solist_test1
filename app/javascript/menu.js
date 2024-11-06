f770c54d7d43b713d4cd3b718acec79c7521089a
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-06 11:11:23 +0900

    JSのrender,load二重起動問題解消

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -2,10 +2,17 @@ function toggleMenu (){
   const menuIcon = document.getElementById("menu-icon");
   const menu = document.getElementById("dropdownMenu");
 
-  menuIcon.addEventListener("click",()=>{
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
-  });
+  // load,render二重起動の阻止
+  if (!menuIcon.hasAttribute('data-listener-added')) {
+
+    menuIcon.addEventListener("click",()=>{
+      menu.style.display = menu.style.display === "block" ? "none" : "block";
+    });
+
+  // load,render二重起動の阻止
+  menuIcon.setAttribute('data-listener-added', 'true');
+  };
 
 };
- window.addEventListener('turbo:load', toggleMenu);
-//  window.addEventListener('turbo:render', toggleMenu);
+window.addEventListener('turbo:load', toggleMenu);
+window.addEventListener('turbo:render', toggleMenu);
