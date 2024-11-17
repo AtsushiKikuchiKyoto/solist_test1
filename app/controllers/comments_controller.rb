df35462b035aff46eb7d0cfac5a14bf91dcf5edd
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-17 21:10:41 +0900

    コメント非同期

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :set_current_profile
+  before_action :set_sound
 
   def create
     Comment.create(comment_params)
     flash[:success] = "コメントをしました。"
-    redirect_to request.referer
+    respond_to do |format|
+      format.turbo_stream #Rail search 'create.turbo_stream.erb'
+      format.html { redirect_to request.referer }
+    end
   end
 
   def destroy
     comment = Comment.find(params[:id])
     flash[:info] = "コメントを削除しました。"
     comment.destroy
-    redirect_to request.referer
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to request.referer }
+    end
   end
 
   private
@@ -19,4 +27,11 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:text).merge(profile_id: session[:current_profile_id], sound_id: params[:sound_id])
   end
 
+  def set_sound
+    @sound = Sound.find(params[:sound_id])
+  end
+
+  def set_current_profile
+    @current_profile = Profile.find(session[:current_profile_id])
+  end
 end
