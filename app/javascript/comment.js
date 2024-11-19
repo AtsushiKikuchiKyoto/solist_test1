35aee0c0d4e601f4d1693381cdb223fd1afa9e52
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-19 18:36:33 +0900

    不要コメント削除

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,17 +1,13 @@
 function comment(){
-
   const soundElements = document.querySelectorAll("[id^='sound_']");
   
-  // 各サウンドアイテムにクリックイベントを追加
   soundElements.forEach(sound => {
     sound.addEventListener("click", () => {
-      // サウンドのIDから番号を取得
       const soundId = sound.id.split("_")[1];
-      // 対応するコメントのIDを作成
       const commentId = `comment_${soundId}`;
       const comment = document.getElementById(commentId);
-      // コメントの表示・非表示を切り替え
       if (comment) {
+        console.log("C")
         if (comment.getAttribute("style") == "display:flex;") {
           comment.setAttribute("style", "display:none;")
         } else {
@@ -24,4 +20,3 @@ function comment(){
 
 };
  window.addEventListener('turbo:load', comment);
-//  window.addEventListener('turbo:render', avatar);
