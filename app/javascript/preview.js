6bf09cf0de1deba24100e92285be03b6238c2371
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-25 10:54:45 +0900

    render時のJS起動機能の追加

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,12 +1,13 @@
-document.addEventListener('turbo:load', function(){
+function preview(){
   if (!document.getElementById('preview')) return null;
 
   const fileField = document.querySelector('input[type="file"]');
   fileField.addEventListener('change', (e)=>{
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-
-    const target = document.getElementById("preview");
-    target.src = blob;
+    const image = document.getElementById("preview");
+    image.src = blob;
   });
-});
+};
+document.addEventListener('turbo:load', preview);
+document.addEventListener('turbo:render', preview);
