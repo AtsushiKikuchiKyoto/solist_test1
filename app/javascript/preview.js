39782153907f1f0e4b6bd36834ac3096414aecba
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-25 10:40:33 +0900

    プレビュー機能の実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,4 +1,12 @@
 document.addEventListener('turbo:load', function(){
   if (!document.getElementById('preview')) return null;
 
+  const fileField = document.querySelector('input[type="file"]');
+  fileField.addEventListener('change', (e)=>{
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const target = document.getElementById("preview");
+    target.src = blob;
+  });
 });
