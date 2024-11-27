32a909f2ebec8e438a524e16bb13484bfc591d3f
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-27 21:18:28 +0900

    再生ボタン機能の改善

--- app/javascript/play.js
+++ app/javascript/play.js
@@ -1,13 +1,15 @@
 function autoplay(){
   const background = document.querySelector(".play");
   const button = document.getElementById("play-button");
+  const audios = document.querySelectorAll("audio");
   let currentIndex = 0;
-  const audioPrefix = "audio_";
-
+  let onAir = false;
+  
   if (!button) return null;
-
+  
+  // 再生が終わったら次のaudioへ
   function playAudio() {
-    const audioElement = document.getElementById(`${audioPrefix}${currentIndex}`);
+    const audioElement = document.getElementById(`audio_${currentIndex}`);
     audioElement.play();
     audioElement.addEventListener("ended", ()=> {
       currentIndex++;
@@ -15,22 +17,34 @@ function autoplay(){
     });
   };
 
-  button.addEventListener("click",()=>{
-    const audios = document.querySelectorAll("audio");
-    let isPlaying = true;
-
-    audios.forEach((audio) => {
-      if (!audio.paused) { 
-        audio.pause(); 
-        isPlaying = false;
-        background.classList.remove("colorful");
-        background.classList.add("grayscale");
-      }
-    });
-    if(isPlaying){
-      playAudio(currentIndex);
+  // ボタンのカラー
+  audios.forEach((audio) => {
+    // 何か再生中
+    audio.addEventListener("play", () => {
       background.classList.remove("grayscale");
       background.classList.add("colorful");
+      onAir = true;
+      currentIndex = audio.id.split("_")[1]; //再生中のID取得
+    });
+    // すべて停止中
+    audio.addEventListener("pause", () => {
+      const allPaused = Array.from(audios).every((audio) => audio.paused);
+      if (allPaused) {
+        background.classList.remove("colorful");
+        background.classList.add("grayscale");
+        onAir = false;
+      }
+    });
+  });
+
+  button.addEventListener("click", ()=>{
+    if(onAir){
+      const audios = document.querySelectorAll("audio");
+      audios.forEach((audio) => {
+        audio.pause();
+      });
+    } else {
+      playAudio(currentIndex);
     };
   });
 
