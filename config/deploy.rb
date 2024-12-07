9eae4cc1deb2d34990e71be00efb96d75b67f5cc
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-12-07 14:19:27 +0900

    test デプロイ

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, 'solist_test1'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:AtsushiKikuchiKyoto/solist_test1.git'
-set :branch, 'main'
+set :branch, 'test'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
